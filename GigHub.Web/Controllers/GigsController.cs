2ebf127846b963338f8f0a65c609245301d46f84
Zhurs11 <soulfly.jah@gmail.com>
2016-06-27 23:03:00 +0300

    Implement saving a geek

--- GigHub.Web/Controllers/GigsController.cs
+++ GigHub.Web/Controllers/GigsController.cs
@@ -5,18 +5,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GigHub.Web.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Web.Controllers
 {
     public class GigsController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
         
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel 
@@ -25,6 +27,24 @@ namespace GigHub.Web.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
 	}
 }
