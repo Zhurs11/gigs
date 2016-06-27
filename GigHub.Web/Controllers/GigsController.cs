13f8b28b423de2d719aff7c933f84242dc736252
Zhurs11 <soulfly.jah@gmail.com>
2016-06-27 19:00:51 +0300

    Create add a Gig form

---
+++ GigHub.Web/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using GigHub.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GigHub.Web.ViewModels;
+
+namespace GigHub.Web.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel 
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+	}
+}
